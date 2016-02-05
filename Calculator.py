1fb1b669b40bff5a2b54f46a50c1071dcb1038f1
lisprolog <peter.sattarpoor@gmx.at>
2016-02-05 17:20:10 +0000

    Python calculator

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+# Calculator.py
+
+def add( a, b):
+	return a + b;
+
+def sub(a, b):
+	return a - b;
+
+def mul(a, b):
+	return a * b;
+
+def div(a, b):
+	return a/b;
+
+def main():
+	print "Zahl1 eingeben: ";
+	print "Zahl2 eingeben: ";
+	print "Operator eingeben: ";
+
+print add(10, 5);
