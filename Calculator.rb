a19a5d808e3513d22d3f10cd60ca4d03dfb1bf8d
lisprolog <peter.sattarpoor@gmx.at>
2016-02-06 22:04:22 +0000

    User input

--- Calculator.rb
+++ Calculator.rb
@@ -7,22 +7,40 @@
 
 #	puts 'Operator eingeben: '
 
+$result = 0
 
-
-def add(a, b)
-	a + b
+def add (a, b)
+	$result = a + b
 end
 
-def sub(a, b)
-	a - b
+def sub (a, b)
+	$result = a - b
 end
 
-def mul(a, b)
-	a * b
+def mul (a, b)
+	$result = a * b
 end
 
-def div(a, b)
-	a / b
+def div (a, b)
+	$result = a / b
 end
 
-print div(10, 5)
+puts "Zahl1 eingeben: "
+number1 = gets.to_i
+
+puts "Zahl2 eingeben: "
+number2 = gets.to_i
+
+puts "Operator eingeben:"
+
+puts "Operator [+]|[-]|[*]|[/] eingeben"
+operator = gets.chomp
+
+case operator
+	when "+" then add(number1, number2);
+	when "-" then sub(number1, number2);
+	when "*" then mul(number1, number2);
+	when "/" then div(number1, number2);
+end
+
+print $result
