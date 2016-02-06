b9b20d6c65b805abad0002b7fbc8df28f56b95fd
lisprolog <peter.sattarpoor@gmx.at>
2016-02-06 21:13:12 +0000

    Ruby calculator

---
+++ Calculator.rb
@@ -0,0 +1,28 @@
+# Calculator.rb
+
+#def calculator()
+#	puts 'Zahl1 eingeben: '
+
+#	puts 'Zahl2 eingeben: '
+
+#	puts 'Operator eingeben: '
+
+
+
+def add(a, b)
+	a + b
+end
+
+def sub(a, b)
+	a - b
+end
+
+def mul(a, b)
+	a * b
+end
+
+def div(a, b)
+	a / b
+end
+
+print div(10, 5)
