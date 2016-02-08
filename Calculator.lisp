47d2a6c634324011a54a5114831ed6f0610fc21f
lisprolog <peter.sattarpoor@gmx.at>
2016-02-08 15:03:10 +0000

    Clisp Calculator

---
+++ Calculator.lisp
@@ -0,0 +1,21 @@
+;calculator.lisp
+
+(defparameter *a* 10)
+
+(defparameter *b* 5)
+
+(defparameter *result* 0)
+
+(defun add (*a* *b*)
+	(+ *a* *b*))
+
+(defun sub (*a* *b*)
+	(- *a* *b*))
+
+(defun mul (*a* *b*)
+	(* *a* *b*))
+
+(defun div (*a* *b*)
+	(/ *a* *b*)) 
+
+	
