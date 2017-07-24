9ba1794889119c762bbeb17d07e981f22b11b66d
lisprolog <lisprolog@users.noreply.github.com>
2017-07-24 21:04:53 +0200

    Perl calculator

--- Calculator.pl
+++ Calculator.pl
@@ -1,19 +1,41 @@
 #!/usr/bin/perl
-#
-#
 
-use strict;
-use warnings;
+print "add  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+print "subt <ENTER> number1 <ENTER> number2 <ENTER>\n";
+print "mul  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+print "div  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+print "exp  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+print "mod  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+print "Operation:";
+$operator = <STDIN>;
+print "number 1: ";
+$m = <STDIN>;
+print "number 2: ";
+$n = <STDIN>;
+print "result: ";
 
-	print "number1: \n";
-	$a = <STDIN>;
-	print "number2: \n";
-	$b = <STDIN>;
-	print "operator: [+][-][*][/]\n";
-	my $o = <STDIN>;
-
-	if($o eq "+\n"){print $a+$b;}
-	if($o eq "-\n"){print $a-$b;}
-	if($o eq "*\n"){print $a*$b;}
-	if($o eq "/\n"){print $a/$b;}
-	print "\n"
+if($operator eq "div\n" && $n == 0){
+	print "Division through 0 not possible!",
+}elsif($operator eq "add\n"){
+	$c = $m + $n;
+}elsif($operator eq "subt\n"){
+	$c = $m - $n;
+}elsif($operator eq "mul\n"){
+	$c = $m * $n;
+}elsif($operator eq "div\n"){
+	$c = $m / $n;
+}elsif($operator eq "exp\n"){
+	$c = $m ** $n;
+}elsif($operator eq "mod\n"){
+	$c = $m % $n;
+}else{
+	print "Error!\n";
+	print "add  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+	print "subt <ENTER> number1 <ENTER> number2 <ENTER>\n";
+	print "mul  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+	print "div  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+	print "exp  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+	print "mod  <ENTER> number1 <ENTER> number2 <ENTER>\n";
+}
+print $c;
+print "\n";
